3c13e39a6c117baa95c301d49f6bd2de4f920f76
Adam A <adamva@outlook.com>
2020-02-11 17:43:03 -0700

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
+const sections = document.querySelectorAll('a.section');
+console.log(sections);
 $(document).ready(() => {
 
     //Hide and show the sidebar
     $('#sidebarCollapse').click(() => {
         $('#sidebar').toggleClass('hidden');
+    });
+
+    $('a.section').click(() => {
+        sections.forEach(section => {
+            $(section).removeClass('active');
+        });
+        $(this).addClass('active');
+        
     });
 });
