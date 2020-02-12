805b2ad80da3c96fe9a84a87470ebd74ed47fc5e
Adam A <adamva@outlook.com>
2020-02-11 19:33:19 -0700

    Add section update as scoll happens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,38 @@
 const sections = document.querySelectorAll('a.section');
-console.log(sections);
 $(document).ready(() => {
 
     //Hide and show the sidebar
     $('#sidebarCollapse').click(() => {
         $('#sidebar').toggleClass('hidden');
     });
+    $('a.section').on('click', function () {
+        toggleActiveSection(sections, this);
+    });
 
-    $('a.section').click(() => {
-        sections.forEach(section => {
-            $(section).removeClass('active');
-        });
-        $(this).addClass('active');
-        
+    $(window).on('resize scroll', function() {
+        if($('section#intro').isInViewport()){
+            toggleActiveSection(sections, $('a#intro-link'));
+        } else if($('section#portfolio').isInViewport()){
+            toggleActiveSection(sections, $('a#portfolio-link'));
+        } else if($('section#about').isInViewport()){
+            toggleActiveSection(sections, $('a#about-link'));
+        } else if($('section#contact').isInViewport()){
+            toggleActiveSection(sections, $('a#contact-link'));
+        }
     });
 });
+
+function toggleActiveSection(links, activeLink) {
+    links.forEach(link =>{
+        $(link).removeClass('active');
+    });
+    $(activeLink).addClass('active');
+}
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementMiddle = elementTop + Math.floor($(this).outerHeight() / 2);
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementMiddle > viewportTop && elementTop < viewportBottom;
+};
